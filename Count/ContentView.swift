15babe5228868bbac3fee57e86c80817e06178fd
Honoka Nishiyama <hono.nish6555@gmail.com>
2025-06-14 10:51:21 +0900

    カウントアレンジ

--- Count/ContentView.swift
+++ Count/ContentView.swift
@@ -10,15 +10,27 @@ import SwiftUI
 struct ContentView: View {
     
     @State var count = 0
+    @State var result = ""
     
     var body: some View {
         VStack {
+            Text("\(result)")
+                .frame(width: 300, height: 100)
+                .font(.system(size: 32))
             Text("\(count)")
                 .frame(width: 300, height: 100)
                 .font(.system(size: 48))
+                .foregroundColor(count >= 0 ? .red : .blue)
             HStack{
                 Button("+"){
                     count = count + 1
+                    if count >= 5 {
+                        result = "プラスの勝ち！"
+                    } else if count <= -5 {
+                        result = "マイナスの勝ち！"
+                    } else {
+                        result = ""
+                    }
                 }
                 .font(.system(size: 48))
                 .foregroundStyle(.white)
@@ -26,6 +38,13 @@ struct ContentView: View {
                 .background(.red)
                 Button("-"){
                     count = count - 1
+                    if count >= 5 {
+                        result = "プラスの勝ち！"
+                    } else if count <= -5 {
+                        result = "マイナスの勝ち！"
+                    } else {
+                        result = ""
+                    }
                 }
                 .font(.system(size: 48))
                 .foregroundStyle(.white)
@@ -40,3 +59,4 @@ struct ContentView: View {
 #Preview {
     ContentView()
 }
+
